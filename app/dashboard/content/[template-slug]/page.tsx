c725f7ea1a0f9e7ff6c58c931a4ec935497fcb8c
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-03-21 16:25:40 +0530

    Create page.tsx

---
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface PROPS{
+    params: {
+        'template-slug': string
+    }
+}
+
+function CreateNewContent(props: PROPS) {
+    return (
+        <div>page</div>
+    );
+}
+
+export default CreateNewContent;
