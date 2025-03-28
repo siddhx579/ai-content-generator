517baff9a1e9b5ed9b8182cac6000228876d6db2
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-03-28 21:37:09 +0530

    Update OutputSection.tsx

--- app/dashboard/content/_components/OutputSection.tsx
+++ app/dashboard/content/_components/OutputSection.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { Editor } from '@toast-ui/react-editor';
 import { Button } from '@/components/ui/button';
@@ -10,6 +10,11 @@ interface props {
 
 function OutputSection({ aiOutput }: props) {
     const editorRef: any = useRef();
+
+    useEffect(() => {
+        const editorInstance = editorRef.current.getInstance();
+        editorInstance.setMarkdown(aiOutput);
+    }, [aiOutput])
 
     return (
         <div className='bg-white shadow-lg border rounded-lg'>
