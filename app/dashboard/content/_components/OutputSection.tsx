534679d79884c8bcb758b6174fcc89c7fd87d491
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-03-28 21:28:41 +0530

    Update OutputSection.tsx

--- app/dashboard/content/_components/OutputSection.tsx
+++ app/dashboard/content/_components/OutputSection.tsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import '@toast-ui/editor/dist/toastui-editor.css';
+import { Editor } from '@toast-ui/react-editor';
+import { Button } from '@/components/ui/button';
+import { Copy } from 'lucide-react';
 
-function OutputSection() {
+interface props {
+    aiOutput: string;
+}
+
+function OutputSection({ aiOutput }: props) {
+    const editorRef:any = useRef();
+
     return (
-        <div>OutputSection</div>
+        <div className='bg-white shadow-lg border rounded-lg'>
+            <div className='flex justify-between items-center p-5'>
+                <h2 className='font-medium text-lg'>Your Result</h2>
+                <Button className='flex gap-2' onClick={() => navigator.clipboard.writeText(aiOutput)}>
+                    <Copy className='w-4 h-4' />
+                    Copy
+                </Button>
+            </div>
+            <Editor
+                ref={editorRef}
+                initialValue="Your result will appear here"
+                previewStyle="vertical"
+                height="600px"
+                initialEditType="wysiwyg"
+                useCommandShortcut={true}
+                onChange={()=>console.log(editorRef.current.getInstance().getMarkdown())}
+            />
+        </div>
     );
 }
 
