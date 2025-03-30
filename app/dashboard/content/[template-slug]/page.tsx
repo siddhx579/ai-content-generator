b6ad578d09e8962c27551a48c495b40110a9517d
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-03-30 19:53:54 +0530

    Update page.tsx

--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { use, useState } from 'react';
+import React, { use, useContext, useState } from 'react';
 import FormSection from '../_components/FormSection';
 import OutputSection from '../_components/OutputSection';
 import Link from 'next/link';
@@ -7,6 +7,7 @@ import { Button } from '@/components/ui/button';
 import { ArrowLeft } from 'lucide-react';
 import Templates from '@/app/(data)/Templates';
 import { TEMPLATE } from '../../_components/TemplateListSection';
+import { chatSession } from '@/utils/AiModal';
 
 interface PROPS {
     params: Promise<{ 'template-slug': string }>;  // params is now a Promise
@@ -19,9 +20,19 @@ function CreateNewContent(props: PROPS) {
         (item) => item.slug === params['template-slug']
     );
     const [aiOutput, setAiOutput] = useState<string>('');
+    // const {totalUsage, setTotalUsage} = useContext(TotalUsageContext);
 
-    const GenerateAIContent = (formData: any) => {
-
+    const GenerateAIContent = async(formData: any) => {
+        // if(totalUsage >= 10000 &! userSubscription){
+        // }
+        setLoading(true);
+        const SelectedPrompt = selectedTemplate?.aiPrompt;
+        const FinalAiPrompt = JSON.stringify(formData) + ", " + SelectedPrompt;
+        const result = await chatSession.sendMessage(FinalAiPrompt);
+        console.log(result.response.text());
+        setAiOutput(result?.response.text());
+        // await SaveInDb(JSON.stringify(formData),selectedTemplate?.slug,result?.response.text());
+        setLoading(false);
     }
 
     return (
@@ -30,7 +41,7 @@ function CreateNewContent(props: PROPS) {
                 <Button><ArrowLeft /> Back</Button>
             </Link>
             <div className='grid grid-cols-1 md:grid-cols-3 gap-5 py-5'>
-                <FormSection selectedTemplate={selectedTemplate} userFormInput={(v: any) => console.log(v)} loading={loading} />
+                <FormSection selectedTemplate={selectedTemplate} userFormInput={(v: any) => GenerateAIContent(v)} loading={loading} />
                 <div className='col-span-2'>
                     <OutputSection aiOutput={aiOutput} />
                 </div>
