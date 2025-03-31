c9c56db44185929c4b5544e374a8b4a4337f9c19
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-03-31 23:11:32 +0530

    Update page.tsx

--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -8,30 +8,56 @@ import { ArrowLeft } from 'lucide-react';
 import Templates from '@/app/(data)/Templates';
 import { TEMPLATE } from '../../_components/TemplateListSection';
 import { chatSession } from '@/utils/AiModal';
+import { PrismaClient } from '@prisma/client';
+import { useUser } from '@clerk/nextjs';
+import moment from 'moment';
 
 interface PROPS {
-    params: Promise<{ 'template-slug': string }>;  // params is now a Promise
+    params: Promise<{ 'template-slug': string }>;
 }
 
 function CreateNewContent(props: PROPS) {
     const [loading, setLoading] = useState(false);
-    const params = use(props.params); // Unwrapping the Promise
+    const params = use(props.params);
     const selectedTemplate: TEMPLATE | undefined = Templates?.find(
         (item) => item.slug === params['template-slug']
     );
     const [aiOutput, setAiOutput] = useState<string>('');
+    const { user } = useUser();
+    const prisma = new PrismaClient();
     // const {totalUsage, setTotalUsage} = useContext(TotalUsageContext);
 
-    const GenerateAIContent = async(formData: any) => {
+    const SaveInDb = async (formData: any, slug: any, aiResp: string) => {
+        try {
+            const response = await fetch('@/app/api/saveContent', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    formData,
+                    templateSlug: slug,
+                    aiResponse: aiResp,
+                    createdBy: user?.primaryEmailAddress?.emailAddress || '',
+                    createdAt: moment().format('YYYY-MM-DD'),
+                }),
+            });
+
+            const result = await response.json();
+            console.log(result);
+        } catch (error) {
+            console.error("Error saving data: ", error);
+        }
+    };
+
+    const GenerateAIContent = async (formData: any) => {
         // if(totalUsage >= 10000 &! userSubscription){
         // }
         setLoading(true);
         const SelectedPrompt = selectedTemplate?.aiPrompt;
         const FinalAiPrompt = JSON.stringify(formData) + ", " + SelectedPrompt;
         const result = await chatSession.sendMessage(FinalAiPrompt);
-        console.log(result.response.text());
+
         setAiOutput(result?.response.text());
-        // await SaveInDb(JSON.stringify(formData),selectedTemplate?.slug,result?.response.text());
+        await SaveInDb(JSON.stringify(formData), selectedTemplate?.slug, result?.response.text());
         setLoading(false);
     }
 
