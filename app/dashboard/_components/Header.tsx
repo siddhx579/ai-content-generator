fe1d9601f949fc6befc6fa696140578c318ddaba
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-04-02 10:34:40 +0530

    Update Header.tsx

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -11,7 +11,7 @@ function Header() {
             </div>
             <div className='flex gap-5 items-center'>
                 <h2 className='bg-primary p-1 rounded-full text-sm text-white px-2'>
-                    🔥 Join Membership just for Rs.99/Month
+                    🔥 Join Membership just for Rs.599/Month
                 </h2>
                 <UserButton />
             </div>
